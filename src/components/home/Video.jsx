58972761622af5624a85944a1091298c73146b77
Aryan Jadhav <jadhav.aryan0702@gmail.com>
2025-09-01 10:20:11 +0530

    Agence hero

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="https://download-video-ak.vimeocdn.com/v3-1/playback/a278bb9c-996c-4760-8ca1-dd0abff308f7/83c745cf?__token__=st=1756638763~exp=1756642363~acl=%2Fv3-1%2Fplayback%2Fa278bb9c-996c-4760-8ca1-dd0abff308f7%2F83c745cf%2A~hmac=70bdcebbf7d5bb11cd229d7427ba16f75519b3213f345bb86def0ba2a2d3df01&r=dXMtd2VzdDE%3D"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted src="\public\video\hero-video.mp4"></video>
     </div>
   )
 }
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 const Agence = () => {
   return (
     <div>
-      Agence
+      <div className='absolute h-[20vw] w-[15vw] rounded-5xl top-60 left-[30vw]'>
+        <img src='https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg'></img>
+      </div>
+      <div className='font-[font2]'>
+        <div className='mt-[55vh]'>
+          <h1 className='text-[20vw] text-center uppercase leading-[17vw]'>Soixan7e <br />
+            Douze</h1>
+        </div>
+        <div className='pl-[40%] mt-20'>
+          <p className='text-6xl '>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux
+            gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité,
+            une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on
+            la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour
+            bâtir des marques influentes.</p>
+        </div>
+      </div>
     </div>
   )
 }
